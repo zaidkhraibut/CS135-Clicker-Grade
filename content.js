b4ee7cc90d77ddc9f7ecea06b1b125903f3f7104
Zaid Khraibut <khraibut.z@gmail.com>
2017-10-18 18:15:29 -0400

    Expanded functionality to also display total assignment grade

--- content.js
+++ content.js
@@ -1,30 +1,71 @@
-// Parses doc for tables, gets mark table, parse for table rows, assigns
-//   variable to each table row
+// Author by: Zaid Khraibut
+// zkhraibu@edu.uwaterloo.ca
 
+
+// Parses doc for tables
 var tables = document.getElementsByTagName("table");
-var clickerTable = tables[1];
-var tableRows = clickerTable.getElementsByTagName("tr");
-var titles = tableRows[0];
-var marksRow = tableRows[1];
 
-// Creates <th>"Total"</th> element and appends to titles row
-var totalTitle = document.createElement("th");
-totalTitle.innerHTML = "Total";
-titles.appendChild(totalTitle);
+
+// Assigns variables to clicker table, 1st row (title) and 2nd row (marks)
+var clickerTable = tables[1];
+var clickerTableRows = clickerTable.getElementsByTagName("tr");
+var clickerTitles = clickerTableRows[0];
+var clickerMarksRow = clickerTableRows[1];
+
+
+// Assigns variable to assignment table
+var assTable = tables[2];
+var assTableRows = assTable.getElementsByTagName("tr");
+
+
+//  Appends any <html tag> and its content to parent
+//  appendCell: <html tag> string row
+//  Example: appendCell("th", "Total", clickerTitles);
+function appendElement(tag, inner, parent) {
+  var cellData = document.createElement(tag);
+  cellData.innerHTML = inner;
+  cellData.style.textAlign = "center";
+  parent.appendChild(cellData);
+};
+
+
+// Appends "Total" to titles row
+appendElement("th", "Total", clickerTitles);
+
 
 // Parses marks row for marks, converts string of each cell to Int, used
 //   toFixed(2) to return percentage at 2 decimal places
-var marks = clickerTable.getElementsByTagName("td");
-var correct = parseInt(marks[0].innerHTML);
-var answered = parseInt(marks[1].innerHTML);
+var clickerMarks = clickerTable.getElementsByTagName("td");
+var correct = parseInt(clickerMarks[0].innerHTML);
+var answered = parseInt(clickerMarks[1].innerHTML);
+var unanswered = parseInt(clickerMarks[2].innerHTML);
 var incorrect = answered - correct;
-var unanswered = parseInt(marks[2].innerHTML);
-var total = (correct * 2) + incorrect;
+var clickerGradeTotal = (correct * 2) + incorrect;
 var totalMarksAvailable = (answered + unanswered) * 2;
-var totalClickerPercentage = ((total/totalMarksAvailable).toFixed(2)) * 100;
+var totalClickerPercentage = ((clickerGradeTotal/totalMarksAvailable).toFixed(2)) * 100;
+var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable + " (" +totalClickerPercentage+"%)";
 
-// Creates <td>...</td> element and appends to titles row
-var totalMarks = document.createElement("td");
-totalMarks.style.textAlign = "center";
-totalMarks.innerHTML = total+"/"+totalMarksAvailable + " (" +totalClickerPercentage+"%)";
-marksRow.appendChild(totalMarks);
+// Appends clickerMarkCellText to clicker grade row
+appendElement("td", clickerMarkCellText, clickerMarksRow);
+
+
+// Add the third element of a specificed row onto the next 3rd element of each
+//   row  until it reaches the the end of the table
+// getAssMarks: Int -> Float
+function getAssMarks(row) {
+  if (row >= assTableRows.length) {
+    return 0;
+  } else {
+    var mark = parseFloat(assTableRows[row].getElementsByTagName("td")[2].innerHTML);
+    return mark + getAssMarks(row + 1);
+  }
+};
+
+// Collects marks starting from the third row, divides mark by total number of rows to determine average
+totalAssMarks = getAssMarks(2);
+var finalAssGrade = totalAssMarks/(assTableRows.length - 2);
+
+// Finds body of assignment table and appends three <td> tags to it with relevent text
+var assTableBody = assTable.getElementsByTagName("tbody")[0];
+var lastAssRowText = "<td><strong>Final</strong></td><td><strong>100</strong></td><td><strong>"+finalAssGrade+"</strong></td>"
+appendElement("tr", lastAssRowText, assTableBody);
