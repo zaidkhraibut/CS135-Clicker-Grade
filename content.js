039dd5c5be28dec0049652db622b817b3eb6766e
Zaid Khraibut <khraibut.z@gmail.com>
2017-10-19 22:22:14 -0400

    Edited description in manifest and made improvement to code style and comments

--- content.js
+++ content.js
@@ -2,8 +2,8 @@
 // zkhraibu@edu.uwaterloo.ca
 
 
-//  Appends any <html tag> and its content to parent
-//  appendCell: <html tag> string row
+//  Appends any <html tag> and its content to a parent
+//  appendCell: <html tag> string <html tag>
 //  Example: appendCell("th", "Total", clickerTitles);
 function appendElement(tag, inner, parent) {
   var cellData = document.createElement(tag);
@@ -27,6 +27,7 @@ var clickerMarksRow = clickerTableRows[1];
 var assTable = tables[2];
 var assTableRows = assTable.getElementsByTagName("tr");
 
+
 // Parses clickerMarksRow for marks, converts string of each cell to Int, used
 // toFixed(2) to return percentage at 2 decimal places
 var clickerMarks = clickerTable.getElementsByTagName("td");
@@ -38,18 +39,22 @@ var clickerGradeTotal = (correct * 2) + incorrect;
 var totalMarksAvailable = (answered + unanswered) * 2;
 var totalClickerPercentage = ((clickerGradeTotal/totalMarksAvailable).toFixed(2)) * 100;
 var weightedClickerMark = ((totalClickerPercentage/75) * 100).toFixed(2);
-var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable;//
+if (weightedClickerMark >= 100) {
+  weightedClickerMark = 100;
+}
+var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable;
+var participationCellText = totalClickerPercentage+"%"+" | <strong>W: "+weightedClickerMark+"</strong>";
 
 // Appends "Total" and " Participation Mark" to titles row
 appendElement("th", "Total", clickerTitles);
 appendElement("th", "Participation Mark", clickerTitles);
 // Appends clickerMarkCellText and totalClickerPercentage to clicker grade row
 appendElement("td", clickerMarkCellText, clickerMarksRow);
-appendElement("td", totalClickerPercentage+"%"+" | <strong>W: "+weightedClickerMark+"</strong>", clickerMarksRow);
+appendElement("td", participationCellText, clickerMarksRow);
 
 
-// Add content of the third element of a specificed row onto the next 3rd element
-// of each row  until it reaches the the end of the table
+// Adds content of the third element of a  onto the next 3rd element of each row
+// until the the end of the table
 // getAssMarks: Int -> Float
 function getAssMarks(row) {
   if (row >= assTableRows.length) {
@@ -62,7 +67,7 @@ function getAssMarks(row) {
 
 // Collects marks starting from the third row, divides mark by total number of rows to determine average
 totalAssMarks = getAssMarks(2);
-var finalAssGrade = totalAssMarks/(assTableRows.length - 2);
+var finalAssGrade = (totalAssMarks/(assTableRows.length - 2)).toFixed(2);
 
 // Finds body of assignment table and appends three <td> tags to it with relevent text
 var assTableBody = assTable.getElementsByTagName("tbody")[0];
