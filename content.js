0a0a7a3749ef4337b916bb7e87fcaf46df105d53
Zaid Khraibut <khraibut.z@gmail.com>
2017-10-19 20:39:27 -0400

    Added weighted participation grade, adjusted table heighted, modified version number

--- content.js
+++ content.js
@@ -2,22 +2,6 @@
 // zkhraibu@edu.uwaterloo.ca
 
 
-// Parses doc for tables
-var tables = document.getElementsByTagName("table");
-
-
-// Assigns variables to clicker table, 1st row (title) and 2nd row (marks)
-var clickerTable = tables[1];
-var clickerTableRows = clickerTable.getElementsByTagName("tr");
-var clickerTitles = clickerTableRows[0];
-var clickerMarksRow = clickerTableRows[1];
-
-
-// Assigns variable to assignment table
-var assTable = tables[2];
-var assTableRows = assTable.getElementsByTagName("tr");
-
-
 //  Appends any <html tag> and its content to parent
 //  appendCell: <html tag> string row
 //  Example: appendCell("th", "Total", clickerTitles);
@@ -29,12 +13,22 @@ function appendElement(tag, inner, parent) {
 };
 
 
-// Appends "Total" to titles row
-appendElement("th", "Total", clickerTitles);
+// Parses doc for tables
+var tables = document.getElementsByTagName("table");
 
+// Assigns variables to clicker table, 1st row (clickerTitle) and 2nd row (clickerMarksRow)
+// and adjusts clicker table width
+var clickerTable = tables[1];
+clickerTable.style.width = "55%";
+var clickerTableRows = clickerTable.getElementsByTagName("tr");
+var clickerTitles = clickerTableRows[0];
+var clickerMarksRow = clickerTableRows[1];
+// Assigns variable to assignment table
+var assTable = tables[2];
+var assTableRows = assTable.getElementsByTagName("tr");
 
-// Parses marks row for marks, converts string of each cell to Int, used
-//   toFixed(2) to return percentage at 2 decimal places
+// Parses clickerMarksRow for marks, converts string of each cell to Int, used
+// toFixed(2) to return percentage at 2 decimal places
 var clickerMarks = clickerTable.getElementsByTagName("td");
 var correct = parseInt(clickerMarks[0].innerHTML);
 var answered = parseInt(clickerMarks[1].innerHTML);
@@ -43,14 +37,19 @@ var incorrect = answered - correct;
 var clickerGradeTotal = (correct * 2) + incorrect;
 var totalMarksAvailable = (answered + unanswered) * 2;
 var totalClickerPercentage = ((clickerGradeTotal/totalMarksAvailable).toFixed(2)) * 100;
-var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable + " (" +totalClickerPercentage+"%)";
+var weightedClickerMark = ((totalClickerPercentage/75) * 100).toFixed(2);
+var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable;//
 
-// Appends clickerMarkCellText to clicker grade row
+// Appends "Total" and " Participation Mark" to titles row
+appendElement("th", "Total", clickerTitles);
+appendElement("th", "Participation Mark", clickerTitles);
+// Appends clickerMarkCellText and totalClickerPercentage to clicker grade row
 appendElement("td", clickerMarkCellText, clickerMarksRow);
+appendElement("td", totalClickerPercentage+"%"+" | <strong>W: "+weightedClickerMark+"</strong>", clickerMarksRow);
 
 
-// Add the third element of a specificed row onto the next 3rd element of each
-//   row  until it reaches the the end of the table
+// Add content of the third element of a specificed row onto the next 3rd element
+// of each row  until it reaches the the end of the table
 // getAssMarks: Int -> Float
 function getAssMarks(row) {
   if (row >= assTableRows.length) {
