4bf7754ab1935278c2c6d61029212927b01f4804
Zaid Khraibut <khraibut.z@gmail.com>
2017-10-23 07:50:59 -0400

    Updated extension to collect midterm marks and assignment marks independently

--- content.js
+++ content.js
@@ -10,14 +10,14 @@ function appendElement(tag, inner, parent) {
   cellData.innerHTML = inner;
   cellData.style.textAlign = "center";
   parent.appendChild(cellData);
-};
+}
 
 
 // Parses doc for tables
 var tables = document.getElementsByTagName("table");
 
 // Assigns variables to clicker table, 1st row (clickerTitle) and 2nd row (clickerMarksRow)
-// and adjusts clicker table width
+//   and adjusts clicker table width
 var clickerTable = tables[1];
 clickerTable.style.width = "55%";
 var clickerTableRows = clickerTable.getElementsByTagName("tr");
@@ -29,7 +29,7 @@ var assTableRows = assTable.getElementsByTagName("tr");
 
 
 // Parses clickerMarksRow for marks, converts string of each cell to Int, used
-// toFixed(2) to return percentage at 2 decimal places
+//   toFixed(2) to return percentage at 2 decimal places
 var clickerMarks = clickerTable.getElementsByTagName("td");
 var correct = parseInt(clickerMarks[0].innerHTML);
 var answered = parseInt(clickerMarks[1].innerHTML);
@@ -37,10 +37,13 @@ var unanswered = parseInt(clickerMarks[2].innerHTML);
 var incorrect = answered - correct;
 var clickerGradeTotal = (correct * 2) + incorrect;
 var totalMarksAvailable = (answered + unanswered) * 2;
-var totalClickerPercentage = ((clickerGradeTotal/totalMarksAvailable).toFixed(2)) * 100;
-var weightedClickerMark = ((totalClickerPercentage/75) * 100).toFixed(2);
+var totalClickerPercentage = clickerGradeTotal/totalMarksAvailable * 100;
+totalClickerPercentage = totalClickerPercentage.toFixed(2);
+var weightedClickerMark = ((totalClickerPercentage/75) * 100);
 if (weightedClickerMark >= 100) {
   weightedClickerMark = 100;
+} else {
+  weightedClickerMark = weightedClickerMark.toFixed(2);
 }
 var clickerMarkCellText = clickerGradeTotal+"/"+totalMarksAvailable;
 var participationCellText = totalClickerPercentage+"%"+" | <strong>W: "+weightedClickerMark+"</strong>";
@@ -53,23 +56,58 @@ appendElement("td", clickerMarkCellText, clickerMarksRow);
 appendElement("td", participationCellText, clickerMarksRow);
 
 
-// Adds content of the third element of a  onto the next 3rd element of each row
-// until the the end of the table
-// getAssMarks: Int -> Float
-function getAssMarks(row) {
+// Definitions used in functions below
+var assignments = Symbol("Assignments");
+var midterms = Symbol("Midterms");
+var mark = 0;
+var numOf = 0;
+var numOfAss = 0;
+var numOfMT = 0;
+
+// Adds together the marks on a row that begins with a specific letter. Updates
+//   numOf to keep track of # of assessments.
+// startsWithLetter should be "M" or "A" for midterm or assignment
+function collectMarks(row, startsWithLetter) {
+  if (assTableRows[row].getElementsByTagName("td")[0].innerHTML.startsWith(startsWithLetter)) {
+    mark = parseFloat(assTableRows[row].getElementsByTagName("td")[2].innerHTML);
+    numOf++;
+  }
+}
+
+// Adds together assignment or midterm marks.
+// Takes the variables assignments, midterms
+function getMarksFn(row, type) {
   if (row >= assTableRows.length) {
     return 0;
-  } else {
-    var mark = parseFloat(assTableRows[row].getElementsByTagName("td")[2].innerHTML);
-    return mark + getAssMarks(row + 1);
+  } else if (type == assignments) {
+      collectMarks(row, "A");
+      numOfAss = numOf;
+  } else if (type == midterms) {
+      collectMarks(row, "M");
+      numOfMT = numOf;
   }
-};
+  return mark + getMarksFn((row + 1), type);
+}
 
-// Collects marks starting from the third row, divides mark by total number of rows to determine average
-totalAssMarks = getAssMarks(2);
-var finalAssGrade = (totalAssMarks/(assTableRows.length - 2)).toFixed(2);
+// Wrapper function for getMarksFn to start counting from the start of marks
+function getMarks(type) {
+  return getMarksFn(2, type);
+}
 
-// Finds body of assignment table and appends three <td> tags to it with relevent text
+// Collectsvtotal number of marks earned and divides by number of that type
+//   of assessment to make a final grade, rounded to 2 decimal places.
+totalAssMarks = getMarks(assignments);
+var finalAssGrade = totalAssMarks/numOfAss;
+finalAssGrade = finalAssGrade.toFixed(2);
+
+totalMTMarks = getMarks(midterms);
+var finalMidtermGrade = totalMTMarks/numOfMT;
+finalMidtermGrade = finalMidtermGrade.toFixed(2);
+
+// Finds body of "Other Marks" table
 var assTableBody = assTable.getElementsByTagName("tbody")[0];
-var lastAssRowText = "<td><strong>Final</strong></td><td><strong>100</strong></td><td><strong>"+finalAssGrade+"</strong></td>"
-appendElement("tr", lastAssRowText, assTableBody);
+// Contents to go inside of <tr> tags that will be appended to assTableBody
+var midtermTotalText = "<td><strong>Midterm Avg.</strong></td><td><strong>100</strong></td><td><strong>"+finalMidtermGrade+"</strong></td>"
+var assignmentTotalText = "<td><strong>Assignment Avg.</strong></td><td><strong>100</strong></td><td><strong>"+finalAssGrade+"</strong></td>"
+appendElement("tr", midtermTotalText, assTableBody);
+appendElement("tr", assignmentTotalText, assTableBody);
